2a8cb82ca5167ea30c8c44a04f4573e4a1345ee7
Chris Chen <chiandeye@gmail.com>
2021-11-20 19:24:10 +0800

    Edit config

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,10 @@
+// module.exports = {
+//   publicPath: process.env.NODE_ENV === 'production'
+//     ? '/tw-bike/'
+//     : '/dist/'
+// }
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production'
     ? '/tw-bike/'
-    : '/dist/'
+    : '/'
 }
